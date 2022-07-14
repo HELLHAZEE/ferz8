4b2d692ec5f0d3e1d274526e68d714958d57e3b2
danilatsm <87435985+danilatsm@users.noreply.github.com>
2022-07-14 23:18:44 +0300

    Add files via upload

---
+++ ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,100 @@
+﻿#include <iostream>
+using namespace std;
+
+bool SVert(int arr[8][8], int i, int j)
+{
+	bool inf = true;
+	for (int i = 0; i < 8; i++)
+	{
+		if (arr[j][i] == 1)
+		{
+			inf = false;
+			return inf;
+		}
+
+	}
+	return inf;
+}
+
+bool SHor(int arr[8][8], int i, int j)
+{
+
+	bool inf = true;
+	for (int j = 0; j < 8; j++)
+	{
+		if (arr[i][j] == 1)
+		{
+			inf = false;
+			return inf;
+		}
+	}
+	return inf;
+}
+
+bool SDiag(int arr[8][8], int i, int j)
+{
+	bool d1 = true, d2 = true, inf = true;
+
+	return inf;
+}
+
+void Nulification(int arr[8][8])
+{
+	for (int i = 0; i < 8; i++)
+		for (int j = 0; j < 8; j++)
+			arr[i][j] = 0;
+}
+
+void Show(int arr[8][8])
+{
+	for (int i = 0; i < 8; i++)
+	{
+		for (int j = 0; j < 8; j++)
+			cout << arr[i][j] << " ";
+		cout << endl;
+	}
+	cout << endl;
+}
+
+void Ferzification(int arr[8][8])
+{
+	int counter = 0;
+	while (counter < 8)
+	{
+		for (int i = 0; i < 8; i++)
+			for (int j = 0; j < 8; j++)
+				if (SVert(arr, i, j) && (SHor(arr, i, j)) && (SDiag(arr, i, j)))
+				{
+					arr[i][j] = 1;
+					counter++;
+				}
+	}
+	cout << "Ferzificated board: " << endl;
+	Show(arr);
+}
+
+int board[8][8]; // Доска; 1 - ферзь
+
+int chk(int c, int r)  // Проверка, что можно ставить в клетку [r][c] 
+{                      // проверка - только слева!
+	for (int i = 0; i < c; ++i)
+	{
+		// Проверка горизонтали
+		if (board[i][r]) return 0;
+		// Проверка диагонали
+		if (r - c + i >= 0) if (board[i][r - c + i]) return 0;
+		if (r + c - i < 8) if (board[i][r + c - i]) return 0;
+	}
+	return 1;
+}
+
+// Сама рекурсивная функция
+int vert(int i) // Установка i-й вертикали
+{
+	
+}
+
+int main(int argc, char* argv[])
+{
+	
+}
