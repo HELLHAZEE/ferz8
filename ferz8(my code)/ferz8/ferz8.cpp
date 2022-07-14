9a1241614a9d97b1ae49aba962c582e359fa9e80
HELLHAZEE <86792432+HELLHAZEE@users.noreply.github.com>
2022-07-14 23:32:15 +0300

    Anishchenko
    add my code program

---
+++ ferz8(my code)/ferz8/ferz8.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+using namespace std;
+
+bool SVert(int arr[8][8], int i, int j)
+{
+	bool inf = true;
+	for (int i = 0; i < 8; i++)
+	{
+		if (arr[j][i] == 1)
+		{
+			inf = false;
+			return inf;
+		}
+
+	}
+	return inf;
+}
+
+bool SHor(int arr[8][8], int i, int j)
+{
+
+	bool inf = true;
+	for (int j = 0; j < 8; j++)
+	{
+		if (arr[i][j] == 1)
+		{
+			inf = false;
+			return inf;
+		}
+	}
+	return inf;
+}
+
+bool SDiag(int arr[8][8], int i, int j)
+{
+	bool d1 = true, d2 = true, inf = true;
+
+	return inf;
+}
+
+void Nulification(int arr[8][8])
+{
+	for (int i = 0; i < 8; i++)
+		for (int j = 0; j < 8; j++)
+			arr[i][j] = 0;
+}
