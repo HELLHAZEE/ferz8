94e1dd65b8a9c8a523e41020602fdd04be8c2cfb
hohgg <95401614+hohgg@users.noreply.github.com>
2022-07-15 10:48:25 +0300

    Update ConsoleApplication21.cpp

--- ConsoleApplication21.cpp
+++ ConsoleApplication21.cpp
@@ -1,44 +1,48 @@
-﻿#void Show(int arr[8][8])
+#include <iostream>
+using namespace std;
+
+bool SVert(int arr[8][8], int i, int j)
 {
+	bool inf = true;
 	for (int i = 0; i < 8; i++)
 	{
-		for (int j = 0; j < 8; j++)
-			cout << arr[i][j] << " ";
-		cout << endl;
+		if (arr[j][i] == 1)
+		{
+			inf = false;
+			return inf;
+		}
+
 	}
-	cout << endl;
+	return inf;
 }
 
-void Ferzification(int arr[8][8])
+bool SHor(int arr[8][8], int i, int j)
 {
-	int counter = 0;
-	while (counter < 8)
+
+	bool inf = true;
+	for (int j = 0; j < 8; j++)
 	{
-		for (int i = 0; i < 8; i++)
-			for (int j = 0; j < 8; j++)
-				if (SVert(arr, i, j) && (SHor(arr, i, j)) && (SDiag(arr, i, j)))
-				{
-					arr[i][j] = 1;
-					counter++;
-				}
+		if (arr[i][j] == 1)
+		{
+			inf = false;
+			return inf;
+		}
 	}
-	cout << "Ferzificated board: " << endl;
-	Show(arr);
+	return inf;
 }
 
-int board[8][8]; // Доска; 1 - ферзь
+bool SDiag(int arr[8][8], int i, int j)
+{
+	bool d1 = true, d2 = true, inf = true;
 
-int chk(int c, int r)  // Проверка, что можно ставить в клетку [r][c] 
-{                      // проверка - только слева!
-	for (int i = 0; i < c; ++i)
-	{
-		// Проверка горизонтали
-		if (board[i][r]) return 0;
-		// Проверка диагонали
-		if (r - c + i >= 0) if (board[i][r - c + i]) return 0;
-		if (r + c - i < 8) if (board[i][r + c - i]) return 0;
-	}
-	return 1;
+	return inf;
+}
+
+void Nulification(int arr[8][8])
+{
+	for (int i = 0; i < 8; i++)
+		for (int j = 0; j < 8; j++)
+			arr[i][j] = 0;
 }
 
 void Show(int arr[8][8])
